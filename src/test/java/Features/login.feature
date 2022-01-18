6cb555b39714d301b9b2737cc83567f7387e04da
l-d-b <laszlo.dora.beata@gmail.com>
2022-01-18 13:39:06 +0100

    add invalid login tests

--- src/test/java/Features/login.feature
+++ src/test/java/Features/login.feature
@@ -1,10 +1,20 @@
 Feature: Application login
 
+  Scenario Outline: login with invalid credentials
+    Given I am on the login page
+    When I fill in Email with <Username>
+    And I fill in Password with <Password>
+    And I press Submit
+    Then I should see warning message <expectedText>
+    Examples:
+      | Username | Password | expectedText |
+      | "test@test.com" | "wrongpassword" | Wrong email address or password was given! Try again.. |
+      | "wrongemail@test.com" | "1234" | Wrong email address or password was given! Try again..|
+
   Scenario: login with valid credentials
     Given I am on the login page
     When I fill in Email with "test@test.com"
     And I fill in Password with "1234"
     And I press Submit
-    Then I should be on the users home page
-    And I should see logged user "Alec Test"
+    Then I should see logged user "Logged user: Alec Test"
     And I should see "LOGOUT" button
--- src/test/java/StepDefinitions/LoginStepDefinitions.java
+++ src/test/java/StepDefinitions/LoginStepDefinitions.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.junit.Cucumber;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
@@ -15,38 +16,38 @@ public class LoginStepDefinitions {
     LoginPage objLoginPage = new LoginPage();
 
     @Given("^I am on the login page$")
-    public void i_am_on_the_login_page() throws Throwable {
+    public void i_am_on_the_login_page() {
         objLoginPage.navigateToLoginPage();
     }
 
     @When("^I fill in Email with \"([^\"]*)\"$")
-    public void i_fill_in_email_with_something(String strArg1) throws Throwable {
-        throw new PendingException();
-    }
-
-    @Then("^I should be on the users home page$")
-    public void i_should_be_on_the_users_home_page() throws Throwable {
-        throw new PendingException();
+    public void i_fill_in_email_with_valid_email(String strArg1) {
+        objLoginPage.setEmail(strArg1);
     }
 
     @And("^I fill in Password with \"([^\"]*)\"$")
-    public void i_fill_in_password_with_something(String strArg1) throws Throwable {
-        throw new PendingException();
+    public void i_fill_in_password_with_valid_password(String strArg1) {
+        objLoginPage.setPassword(strArg1);
     }
 
     @And("^I press Submit$")
-    public void i_press_submit() throws Throwable {
-        throw new PendingException();
+    public void i_press_submit() {
+        objLoginPage.pressSubmit();
     }
 
-    @And("^I should see logged user \"([^\"]*)\"$")
-    public void i_should_see_logged_user_something(String strArg1) throws Throwable {
-        throw new PendingException();
+    @Then("^I should see warning message (.+)$")
+    public void i_should_see_warning_message(String expectedtext) {
+        Assertions.assertEquals(objLoginPage.getWarning(), expectedtext);
+    }
+
+    @Then("^I should see logged user \"([^\"]*)\"$")
+    public void i_should_see_logged_user_name(String strArg1) {
+        Assertions.assertEquals(objLoginPage.getLoggedUser(), strArg1);
     }
 
     @And("^I should see \"([^\"]*)\" button$")
-    public void i_should_see_something_button(String strArg1) throws Throwable {
-        throw new PendingException();
+    public void i_should_see_something_button(String strArg1) {
+        Assertions.assertEquals(objLoginPage.getLoggingButtonText(), strArg1);
     }
 
 }
