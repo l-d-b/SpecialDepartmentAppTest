fbec4e5c3da10f82383a619d2c2573020c6a33e5
Tuba05 <bbence925@gmail.com>
2022-01-19 22:03:00 +0100

    [#20] create MainPageProfession class and its fields and basic methods

---
+++ src/test/java/PageFactory/MainPageWorkObject.java
@@ -0,0 +1,36 @@
+package PageFactory;
+
+import click.webelement.pagefactory.parameterized.DefaultParameterProvider;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MainPageWorkObject extends BasePage{
+
+    @FindBy(xpath = "//div[@id='sidebar']//div[text()='Extra Search']")
+    WebElement extraSearchMenuPoint;
+
+    @FindBy(xpath = "//div[@id='workObjectExtraSearch']")
+    WebElement workObjectMenuPoint;
+
+    @FindBy(xpath = "//input[@id='{wec:input.text}']")
+    WebElement workObject;
+
+
+
+    public MainPageWorkObject(String workObject) {
+        DefaultParameterProvider.properties.set(Map.of("li.text", workObject));
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
+    }
+
+    public int returnNumberOfWorkersWhereProfessionIsAppeared(){
+        List<WebElement> workers = driver.findElements(By.xpath("//div[@class='worker-card']"));
+        return workers.size();
+    }
+}
