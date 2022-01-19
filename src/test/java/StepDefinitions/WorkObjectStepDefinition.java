f47d2e1c2940d2011da4f77d1b2b1ee0a68598cd
Tuba05 <bbence925@gmail.com>
2022-01-19 22:23:06 +0100

    [#19] implement function in methods

--- src/test/java/StepDefinitions/WorkObjectStepDefinition.java
+++ src/test/java/StepDefinitions/WorkObjectStepDefinition.java
@@ -1,31 +1,36 @@
 package StepDefinitions;
 
 
+import PageFactory.MainPageWorkObject;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(Cucumber.class)
 public class WorkObjectStepDefinition {
+    MainPageWorkObject mainPageWorkObject = new MainPageWorkObject("");
 
     @Then("^User click on Extra Search menu point$")
     public void user_click_on_extra_search_menu_point() throws Throwable {
-
+        mainPageWorkObject.clickOnExtraSearchMenuPoint();
     }
 
-    @Then("^User click on a workObject (.+)$")
-    public void user_click_on_a_workobject(String workobject) throws Throwable {
-
+    @Then("^User click on a workObject$")
+    public void user_click_on_a_workobject() throws Throwable {
+        mainPageWorkObject.clickOnWorkObjectMenuPoint();
     }
 
     @And("^User click on Work Object menu point$")
     public void user_click_on_work_object_menu_point() throws Throwable {
-
+        mainPageWorkObject.clickOnRadioButton();
     }
 
-    @And("^Chek that the proper workers show up (.+) (.+)$")
-    public void chek_that_the_proper_workers_show_up(String workobject, String numberofworker) throws Throwable {
-
+    @And("^Chek that the proper workers show up (.+)$")
+    public void chek_that_the_proper_workers_show_up(int numberofworker) throws Throwable {
+        int actualNumberOfWorker = mainPageWorkObject.returnNumberOfWorkersWhereProfessionIsAppeared();
+        assertEquals(numberofworker, actualNumberOfWorker);
     }
 }
