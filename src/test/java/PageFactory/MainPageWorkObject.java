c25599ce15a912aac3feb9b9088a49f825e78922
Tuba05 <bbence925@gmail.com>
2022-01-19 22:05:44 +0100

    [#20] add clickOnRadioButton method

--- src/test/java/PageFactory/MainPageWorkObject.java
+++ src/test/java/PageFactory/MainPageWorkObject.java
@@ -3,6 +3,7 @@ package PageFactory;
 import click.webelement.pagefactory.parameterized.DefaultParameterProvider;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 public class MainPageWorkObject extends BasePage{
+    Actions actions = new Actions(driver);
 
     @FindBy(xpath = "//div[@id='sidebar']//div[text()='Extra Search']")
     WebElement extraSearchMenuPoint;
@@ -32,5 +34,10 @@ public class MainPageWorkObject extends BasePage{
     public int returnNumberOfWorkersWhereProfessionIsAppeared(){
         List<WebElement> workers = driver.findElements(By.xpath("//div[@class='worker-card']"));
         return workers.size();
+    }
+
+    public void clickOnRadioButton(){
+        actions.moveToElement(workObjectMenuPoint).click().perform();
+
     }
 }
