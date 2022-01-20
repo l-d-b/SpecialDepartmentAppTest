847d0195c83ba20e13a8a46f9397c59a637ca71d
Tuba05 <bbence925@gmail.com>
2022-01-20 11:27:45 +0100

    [#23] add try-catch and refactor

--- src/test/java/PageFactory/MainPageNameSearch.java
+++ src/test/java/PageFactory/MainPageNameSearch.java
@@ -1,6 +1,8 @@
 package PageFactory;
 
 import net.bytebuddy.implementation.bytecode.ShiftRight;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -16,7 +18,7 @@ public class MainPageNameSearch extends BasePage{
     @FindBy(xpath = "//input[@id='nameSearch']")
     WebElement searchField;
 
-    @FindBy(xpath = "//div[@id='worker-card-container']//div[@class='worker-card']//h3")
+    @FindBy(xpath = "//div[@id='worker-card-container']//div[@class='worker-card']//h3[contains(text(),' + input + ')]")
     WebElement workerName;
 
     @FindBy(xpath = "//p[@id='no-worker-found']")
@@ -35,19 +37,25 @@ public class MainPageNameSearch extends BasePage{
         searchField.sendKeys(name);
     }
 
-    public String getWorkerName() {
-        return workerName.getAttribute("innerText");
+    public String getWorkerName(String input) {
+        try {
+            return driver.findElement(By.xpath("//div[@id='worker-card-container']//div[@class='worker-card']//h3[contains(text(),'" + input + "')]")).getText();
+        } catch (NoSuchElementException e) {
+            System.out.println("Element not exists");
+            return getNoWorkerFoundMessage();
+        }
     }
 
     public String getNoWorkerFoundMessage() {
         return noWorkerFoundMessage.getText();
     }
 
-    public String actualWorker(){
-        if(!(this.getWorkerName().equals(""))){
-            return getNoWorkerFoundMessage();
+    public String actualWorker(String name){
+        if(getWorkerName(name).contains(name)){
+            return getWorkerName(name);
+
         } else {
-            return getWorkerName();
+            return getNoWorkerFoundMessage();
         }
     }
 
