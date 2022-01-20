8f84b8a0c45d28786fb4d41c7b080de95aafb05d
Tuba05 <bbence925@gmail.com>
2022-01-20 10:44:57 +0100

    [#23] add WebElement fields

--- src/test/java/PageFactory/MainPageNameSearch.java
+++ src/test/java/PageFactory/MainPageNameSearch.java
@@ -1,9 +1,23 @@
 package PageFactory;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
+import java.util.List;
+
 public class MainPageNameSearch extends BasePage{
+
+    @FindBy(xpath = "//div[@id='sidebar']//div[text()='Extra Search']")
+    WebElement extraSearchMenuPoint;
+
+    @FindBy(xpath = "//input[@id='nameSearch']")
+    WebElement searchField;
+
+    @FindBy(xpath = "//div[@id='worker-card-container']//div[@class='worker-card']")
+    WebElement worker;
+
 
     public MainPageNameSearch() {
         PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
