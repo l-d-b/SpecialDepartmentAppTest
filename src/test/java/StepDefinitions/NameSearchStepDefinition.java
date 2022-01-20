0ea4ee91876076d00613f122c6ab86c09e14355b
Tuba05 <bbence925@gmail.com>
2022-01-20 11:27:13 +0100

    [#18] fill up methods

--- src/test/java/StepDefinitions/NameSearchStepDefinition.java
+++ src/test/java/StepDefinitions/NameSearchStepDefinition.java
@@ -5,16 +5,20 @@ import io.cucumber.java.en.Then;
 import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(Cucumber.class)
 public class NameSearchStepDefinition {
     MainPageNameSearch mainPageNameSearch = new MainPageNameSearch();
 
     @Then("^User write in the Name Search field (.+)$")
     public void user_write_in_the_name_search_field(String name) throws Throwable {
-
+        mainPageNameSearch.sendKeyToSearchField(name);
     }
 
     @Then("^Check that the proper workers show up (.+)$")
-    public void check_that_the_proper_workers_show_up(String name) throws Throwable {
+    public void check_that_the_proper_workers_show_up(String expectedWorker) throws Throwable {
+        String actualWorker = mainPageNameSearch.actualWorker(expectedWorker);
+        assertEquals(actualWorker, expectedWorker);
     }
 }
