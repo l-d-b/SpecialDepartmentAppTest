1e126193469350991b96143a6c20263456309ca3
Tuba05 <bbence925@gmail.com>
2022-01-20 09:50:51 +0100

    [#20] refactor clickOnRadioButton

--- src/test/java/PageFactory/MainPageWorkObject.java
+++ src/test/java/PageFactory/MainPageWorkObject.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -26,15 +28,18 @@ public class MainPageWorkObject extends BasePage{
     @FindByParameterized(xpath = "//input[@id='{wec:id.text}']")
     WebElement workObject;
 
+    @FindBy(xpath = "//div[@id='worker-card-container']//div[@class='worker-card']")
+    List<WebElement> workers;
+
 
 
     public MainPageWorkObject(String workObject) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT, POLLING));
         DefaultParameterProvider.properties.set(Map.of("id.text", workObject));
         PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
     }
 
-    public int returnNumberOfWorkersWhereProfessionIsAppeared(){
-        List<WebElement> workers = driver.findElements(By.xpath("//div[@class='worker-card']"));
+    public int returnNumberOfWorkersWhereProfessionIsAppeared() {
         return workers.size();
     }
 
@@ -46,8 +51,8 @@ public class MainPageWorkObject extends BasePage{
         driver.findElement(By.xpath("//div[@id='workObjectExtraSearch']")).click();
     }
 
-    public void clickOnRadioButton(){
-        Actions actions = new Actions(driver);
-        actions.moveToElement(workObject).click().perform();
+    public void clickOnRadioButton() throws InterruptedException {
+        workObject.click();
+        wait.until(ExpectedConditions.elementSelectionStateToBe(workObject, true));
     }
 }
